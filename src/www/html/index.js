0d9e1385d76c1093e42dfb738df3880da881259b
Tarsis <br.tmvdl@gmail.com>
2023-12-15 11:32:44 -0300

    Updating www files

--- src/www/html/index.js
+++ src/www/html/index.js
@@ -1,12 +1,7 @@
-import { HTML, nInput, nButton } from '@brtmvdl/frontend'
+import { HTML } from '@brtmvdl/frontend'
 
-const app = HTML.fromId('app')
-
-const input = new nInput()
-input.setPlaceholder('input')
-app.append(input)
-
-const button = new nButton()
-button.setText('button')
-button.on('click', () => window.alert(`value: ${input.getValue()}`))
-app.append(button)
+export class Page extends HTML {
+  onCreate() {
+    this.setText('page')
+  }
+}
