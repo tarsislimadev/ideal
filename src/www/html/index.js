0e7164fdbd7580a2c20b1d72d474e11a77a4aab3
Tarsis <br.tmvdl@gmail.com>
2024-01-23 11:12:18 -0300

    Updating app files

--- src/www/html/index.js
+++ src/www/html/index.js
@@ -1,17 +1,5 @@
-import { HTML, nLink } from '@brtmvdl/frontend'
+import { HTML } from '@brtmvdl/frontend'
 
-export class Page extends HTML {
-  onCreate() {
-    this.append(this.createLink('home', '/'))
-    Array.from(['register', 'login', 'dashboard', 'create-app', 'build-app', 'publish-app']).map((page) => {
-      this.append(this.createLink(page, `/pages/${page}/`))
-    })
-  }
+import { PageComponent } from './components/index.js'
 
-  createLink(text, href) {
-    const link = new nLink()
-    link.setText(text)
-    link.href(href)
-    return link
-  }
-}
+export class Page extends PageComponent { }
--- src/www/html/pages/login/index.js
+++ src/www/html/pages/login/index.js
@@ -1,4 +1,6 @@
-import { HTML, nError, nInputTextGroup, nButton, nLink } from '@brtmvdl/frontend'
+import { HTML, nH2, nError, nInputTextGroup, nButton, nLink } from '@brtmvdl/frontend'
+
+import { ButtonComponent, InputTextGroupComponent, LinkComponent, PageComponent } from '../../components/index.js'
 
 import * as API from '../../utils/api.js'
 
@@ -6,27 +8,33 @@ import * as Flow from '../../utils/flow.js'
 
 import * as Local from '../../utils/local.js'
 
-export class Page extends HTML {
+export class Page extends PageComponent {
   children = {
     error_message: new nError(),
-    user_input: new nInputTextGroup(),
-    password_input: new nInputTextGroup(),
-    login_button: new nButton(),
-    register_link: new nLink()
+    user_input: new InputTextGroupComponent(),
+    password_input: new InputTextGroupComponent(),
+    login_button: new ButtonComponent(),
+    register_link: new LinkComponent()
   }
 
-  onCreate() {
-    this.append(this.getForm())
-  }
-
-  getForm() {
+  getContent() {
     const form = new HTML()
+    form.setStyle('margin', '0 auto')
+    form.setStyle('width', '10rem')
+    form.append(this.getTitle('Login'))
     form.append(this.getErrorMessage())
     form.append(this.getUserInput())
     form.append(this.getPasswordInput())
     form.append(this.getLoginButton())
     form.append(this.getRegisterLink())
     return form
+  }
+
+  getTitle(title) {
+    const h2 = new nH2()
+    h2.setStyle('text-align', 'center')
+    h2.setText(title)
+    return h2
   }
 
   getErrorMessage() {
@@ -40,6 +48,7 @@ export class Page extends HTML {
 
   getPasswordInput() {
     this.children.password_input.children.label.setText('password')
+    this.children.password_input.children.input.setAttr('type', 'password')
     return this.children.password_input
   }
 
@@ -62,7 +71,9 @@ export class Page extends HTML {
 
   getRegisterLink() {
     this.children.register_link.setText('register')
-    this.children.register_link.href('/register/')
+    this.children.register_link.href('/pages/register/')
+    this.children.register_link.setStyle('text-align', 'center')
+    this.children.register_link.setStyle('width', '100%')
     return this.children.register_link
   }
 }
