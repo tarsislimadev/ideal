e3bceee1691df0a436d62bc4495f13936fb3c280
Tarsis <br.tmvdl@gmail.com>
2024-04-11 18:33:55 -0300

    Updating app files

--- src/app/html/pages/login/index.js
+++ src/app/html/pages/login/index.js
@@ -1,36 +1,29 @@
-import { HTML, nH2, nError, nInputTextGroup, nButton, nLink } from '@brtmvdl/frontend'
-
-import { ButtonComponent, InputTextGroupComponent, LinkComponent, PageComponent } from '../../components/index.js'
-
-import * as API from '../../utils/api.js'
-
+import { HTML, nHr, nH2, nError } from '@brtmvdl/frontend'
+import { ButtonComponent, InputTextGroupComponent, PageComponent } from '../../components/index.js'
 import * as Flow from '../../utils/flow.js'
-
-import * as Local from '../../utils/local.js'
 
 export class Page extends PageComponent {
   children = {
     error_message: new nError(),
-    user_input: new InputTextGroupComponent(),
-    password_input: new InputTextGroupComponent(),
-    login_button: new ButtonComponent(),
-    register_link: new LinkComponent()
+    email_input: new InputTextGroupComponent(),
   }
 
   getContent() {
     const form = new HTML()
     form.setStyle('margin', '0 auto')
     form.setStyle('width', '10rem')
-    form.append(this.getTitle('Login'))
+    form.append(this.createH2('Login'))
     form.append(this.getErrorMessage())
-    form.append(this.getUserInput())
-    form.append(this.getPasswordInput())
+    form.append(this.getGoogleButton())
+    form.append(this.getGitlabButton())
+    form.append(this.getBitBucketButton())
+    form.append(this.getHr())
+    form.append(this.getEmailInput())
     form.append(this.getLoginButton())
-    form.append(this.getRegisterLink())
     return form
   }
 
-  getTitle(title) {
+  createH2(title) {
     const h2 = new nH2()
     h2.setStyle('text-align', 'center')
     h2.setText(title)
@@ -41,29 +34,35 @@ export class Page extends PageComponent {
     return this.children.error_message
   }
 
-  getUserInput() {
-    this.children.user_input.children.label.setText('user')
-    return this.children.user_input
+  getGoogleButton() {
+    return this.createFlowButton('google', () => Flow.goTo('?google=1'))
   }
 
-  getPasswordInput() {
-    this.children.password_input.children.label.setText('password')
-    this.children.password_input.children.input.setAttr('type', 'password')
-    return this.children.password_input
+  getGitlabButton() {
+    return this.createFlowButton('gitlab', () => Flow.goTo('?gitlab=1'))
+  }
+
+  getBitBucketButton() {
+    return this.createFlowButton('bitbucket', () => Flow.goTo('?bitbucket=1'))
+  }
+
+  getHr() {
+    return new nHr()
+  }
+
+  getEmailInput() {
+    this.children.email_input.children.input.setPlaceholder('email')
+    return this.children.email_input
+  }
+
+  createFlowButton(text, onClick = (() => { })) {
+    const button = new ButtonComponent()
+    button.setText(text)
+    button.on('click', () => onClick())
+    return button
   }
 
   getLoginButton() {
-    this.children.login_button.setText('login')
-    this.children.login_button.on('click', () => Flow.goTo('/pages/dashboard/'))
-
-    return this.children.login_button
-  }
-
-  getRegisterLink() {
-    this.children.register_link.setText('register')
-    this.children.register_link.href('/pages/register/')
-    this.children.register_link.setStyle('text-align', 'center')
-    this.children.register_link.setStyle('width', '100%')
-    return this.children.register_link
+    return this.createFlowButton('login', () => Flow.goTo('/pages/dashboard/'))
   }
 }
