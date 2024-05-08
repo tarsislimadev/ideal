78880ce02bf6a172ad19043cedbc453d2b788e7b
Tarsis <br.tmvdl@gmail.com>
2024-05-08 17:11:44 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,12 @@ const server = new Server()
 
 const db = new Database({ type: 'fs', config: '/data' })
 
-server.get('/', (req, res) => res.setJSON({ endpoint: 'index', id: Date.now() }))
+server.get('/', (_, res) => {
+  return res.setJSON({ id: Date.now() })
+})
 
-server.get('/dashboard', (req, res) => res.setJSON({ endpoint: 'dashboard', id: Date.now() }))
+server.get('/dashboard', (_, res) => {
+  return res.setJSON({ id: Date.now() })
+})
 
 server.listen(80)
