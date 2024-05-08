3d06d8ab58c819a5a25ff1a445a638aad1c44fd6
Tarsis <br.tmvdl@gmail.com>
2024-05-08 18:01:54 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,21 @@
-import { Server } from 'express'
-
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 import { Database } from '@brtmvdl/database'
 
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
 const db = new Database({ type: 'fs', config: '/data' })
 
-const setHeaders = (res, error = null, json = {}) => res.setJSON(json).setHeader('Access-Control-Allow-Origin', '*')
+const setHeaders = (res, error = null, json = {}) => res
+  .set('Access-Control-Allow-Origin', '*')
+  .send(json)
 
-const server = new Server()
+io.on('connection', (socket) => console.log('connection', socket))
 
-server.get('/', (_, res) => setHeaders(res, null, { id: Date.now() }))
+app.get('/', (_, res) => setHeaders(res, null, { id: Date.now() }))
 
-server.get('/dashboard', (_, res) => setHeaders(res, null, { projects: [Date.now()] }))
+app.get('/dashboard', (_, res) => setHeaders(res, null, { projects: [Date.now()] }))
 
-server.listen(80)
+server.listen(80, () => console.log('server running at http://localhost:80/'))
