f97d19f1e1b80af35dc35d2796987c28d22195ab
Tarsis <br.tmvdl@gmail.com>
2024-05-08 17:38:13 -0300

    Updating app files

--- src/app/html/pages/dashboard/index.js
+++ src/app/html/pages/dashboard/index.js
@@ -1,3 +1,9 @@
 import { PageComponent } from '../../components/index.js'
 
-export class Page extends PageComponent { }
+import * as API from '../../utils/api.js'
+
+export class Page extends PageComponent {
+  onCreate() {
+    API.dashboard().then((res) => console.log('getData', res.getData()))
+  }
+}
